3eba63f986242923344c9f2708f0dff1179f833d
ilya777grin23 <IRuss23@yandex.ru>
2018-10-08 16:51:15 +0300

    Fix naming. Add comments. Fix readme.

--- public/SnakeGame.js
+++ public/SnakeGame.js
@@ -2,13 +2,17 @@ class SnakeGame {
     constructor(canv) {
         this.canv = canv
         this.ctx = this.canv.getContext('2d');
+
+        // Don't afraid, that gen array of arr[20][20] :)
         this.matrix = (('0'.repeat(20) + '\n').repeat(20))
             .split('\n')
             .map(e => e.split('').map(e => +e))
             .slice(0, 20);
 
+        // Main snake
         this.snake = {
-            isAlife: true,
+            isAlive: true,
+            // directory
             dirs: {
                 x: -1,
                 y: 0
@@ -20,63 +24,67 @@ class SnakeGame {
                 [9, 9],
                 [9, 10]
             ],
+            // requestAnimationFrame timeOut diff
             diff: 0,
+            // move to head dir
             moveAhead(matrix, appleGen) {
                 let { pos } = this;
-                // 0.3s
+                // ms
                 if (this.diff >= 40) {
                     const { x, y } = this.dirs;
 
-                    const lastIndex = pos[pos.length - 1];
-                    matrix[lastIndex[0]][lastIndex[1]] = 0;
+                    const lastElem = pos[pos.length - 1];
+                    matrix[lastElem[0]][lastElem[1]] = 0;
                     
+                    // Previos pos of head
                     let prev = pos[0];
                     this.pos[0] = [pos[0][0] + y, pos[0][1] + x];
 
+                    // Creat loop of area
                     const p = this.pos;
-
                     if (p[0][0] >= 20) {
                         this.pos[0][0] = 0;
-                    }
-
-                    if (p[0][1] >= 20) {
+                    } else if (p[0][1] >= 20) {
                         this.pos[0][1] = 0;
-                    }
-
-                    if (p[0][0] < 0) {
+                    } else if (p[0][0] < 0) {
                         this.pos[0][0] = 19;
-                    }
-
-                    if (p[0][1] < 0) {
+                    } else if (p[0][1] < 0) {
                         this.pos[0][1] = 19;
                     }
 
+                    // Replace all position to next position
                     for (let i = 1; i < this.pos.length; i++) {
                         [prev, this.pos[i]] = [this.pos[i], prev];
                     }
 
+                    // If snake has eaten apple
                     if (this.nextTail) {
                         this.pos.push(this.nextTail);
                         this.nextTail = null;
                     }
 
+                    // Check next position of head
                     switch (matrix[this.pos[0][0]][this.pos[0][1]]) {
+                        // If that apple
                         case 2: 
                             this.nextTail = this.pos[this.pos.length - 1];
                             appleGen();
                             break;
+                        // If that body
                         case 1:
-                            this.isAlife = false;
+                            this.isAlive = false;
                             break;
                     }
 
-                    if (!this.isAlife) return;
+                    // If snake not alive, exit from function
+                    if (!this.isAlive) return;
 
                     this.diff = 0;
                 } else {
                     this.diff += 1;
                 }
 
+                // Rewrite matrix
                 pos.forEach((c, i) => {
                     matrix[c[0]][c[1]] = i === 0 ? 3 : 1;
                 });
@@ -92,10 +100,12 @@ class SnakeGame {
 
     init() {
         this.appleGen();
-        this.events();
+        this.eventsInit();
         this.update();
     }
 
+    // Generate random position for apple,
+    // fow empty space
     appleGen() {
         const emptyCoords = [];
         this.matrix.forEach((row, y) => 
@@ -108,11 +118,13 @@ class SnakeGame {
         const coords = emptyCoords[Math.random() * len | 0];
 
         this.matrix[coords[0]][coords[1]] = 2;
+        return coords;
     }
 
-    events() {
+    eventsInit() {
         document.addEventListener('keydown', event => {
             event.preventDefault();
+            // Set direction for snake, on key...
             switch (event.key) {
                 case 'ArrowUp':
                 this.snake.diff = 1000;
@@ -138,23 +150,29 @@ class SnakeGame {
         });
     }
 
+    showScore() {
+        const { ctx, metrika: m } = this;
+
+        ctx.fillStyle = '#000';
+        ctx.strokeStyle = '#fff';
+
+        // length - 3, be-se, on game start, snake have 2 body, and 1 head
+        const score = `Score: ${(this.snake.pos.length - 3) * 100}`;
+
+        const fontSize = m.fr.width / score.length * 20;
+        ctx.font = `bold ${fontSize}px Arial`;
+
+        ctx.fillText(score, m.fr.width / 2 + fontSize, m.fr.height / 2 + fontSize);
+        ctx.strokeText(score, m.fr.width / 2 + fontSize, m.fr.height / 2 + fontSize);
+    }
+
     update() {
         this.snake.moveAhead(this.matrix, this.appleGen);
         this.draw();
 
-        const { ctx, metrika: m } = this;
-        if (!this.snake.isAlife) {
-            ctx.fillStyle = '#000';
-            ctx.strokeStyle = '#fff';
-
-            const score = `Score: ${(this.snake.pos.length - 3) * 100}`;
-
-            const fontSize = m.fr.width / score.length * 20;
-            ctx.font = `bold ${fontSize}px Arial`;
-           
-
-            ctx.fillText(score, m.fr.width / 2 + fontSize, m.fr.height / 2 + fontSize);
-            ctx.strokeText(score, m.fr.width / 2 + fontSize, m.fr.height / 2 + fontSize);
+        // Show score, after die
+        if (!this.snake.isAlive) {
+            this.showScore();
             return;
         }
 
@@ -163,6 +181,7 @@ class SnakeGame {
 
     draw() {
         const { ctx, canv } = this;
+        // Rewrite metrika, on every frame
         this.metrika = {
             width: canv.width,
             height: canv.height,
@@ -173,8 +192,8 @@ class SnakeGame {
             }
         };
 
-        const { metrika: m, matrix } = this;
-
+        // Draw all blocks
+        const { matrix } = this;
         matrix.forEach((row, y) => 
             row.forEach((cell, x) => {
                 this.drawFraction(x, y);
@@ -182,6 +201,7 @@ class SnakeGame {
         );
     }
 
+    // Draw one block
     drawFraction(x, y) {
         // Define all vars
         const { matrix, ctx, metrika: m } = this;
@@ -279,5 +299,7 @@ class SnakeGame {
                 );
                 break;
         }
+
+        return matrix[y][x];
     }
 }
